93ab694909e2b841a3f90c3b9e3002c4949705c9
Henry <henry.alakazhang@gmail.com>
2023-05-05 16:38:22 +1000

    Refactor Physics logic into its own base class

--- src/objects/projectile.ts
+++ src/objects/projectile.ts
@@ -1,77 +1,7 @@
-import { Graphics, Ticker } from "pixi.js";
+import { PhysicsObject } from "./physics-object";
 
-/**
- * A basic graphic object with movement physics
- */
-export class Projectile extends Graphics {
-  private velocityX = 0;
-  private velocityY = 0;
-  private accelX = 0;
-  private accelY = 0;
-
-  /** Amount the acceleration decreases by over time, as a percentage. */
-  private friction = 0;
-
-  /** Whether the projectile should rotate in the direction it's going */
-  private shouldRotate = false;
-
-  private ticker: Ticker;
-
+export class Projectile extends PhysicsObject {
   constructor() {
     super();
-
-    this.ticker = new Ticker().add((delta: number) => this.update(delta));
-    this.ticker.start();
-  }
-
-  public setVelocity(x: number, y: number): this {
-    this.velocityX = x;
-    this.velocityY = y;
-    this.updateRotation();
-    return this;
-  }
-
-  public setAcceleration(x: number, y: number): this {
-    this.accelX = x;
-    this.accelY = y;
-    return this;
-  }
-
-  public setFriction(friction: number): this {
-    this.friction = friction;
-    return this;
-  }
-
-  public setRotatable(rotatable: boolean): this {
-    this.shouldRotate = rotatable;
-    this.updateRotation();
-    return this;
-  }
-
-  private update(delta: number): void {
-    // console.log(this.x, this.y, this.velocityX, this.velocityY);
-
-    this.x = this.x + this.velocityX * delta;
-    this.y = this.y + this.velocityY * delta;
-
-    this.velocityX = this.velocityX + this.accelX * delta;
-    this.velocityY = this.velocityY + this.accelY * delta;
-
-    this.accelX = this.accelX * (1 - this.friction) * delta;
-    this.accelY = this.accelY * (1 - this.friction) * delta;
-
-    this.updateRotation();
-  }
-
-  private updateRotation() {
-    // update angle based on movement
-    if (this.shouldRotate) {
-      this.rotation = Math.atan2(this.velocityY, this.velocityX);
-    }
-  }
-
-  override destroy() {
-    this.ticker.stop();
-    return super.destroy();
   }
 }
