5f94b4c1fc698bc6fb1766f775650ac094af956b
Henry <henry.alakazhang@gmail.com>
2023-05-05 20:09:33 +1000

    Add todos related to music

---
+++ src/services/music.ts
@@ -0,0 +1,23 @@
+/**
+ * Menu Theme
+ * "Space Fighter Loop" Kevin MacLeod (incompetech.com)
+ * Licensed under Creative Commons: By Attribution 4.0 License
+ * http://creativecommons.org/licenses/by/4.0/
+ *
+ * Battle Theme 1
+ * "Shiny Tech" Kevin MacLeod (incompetech.com)
+ * Licensed under Creative Commons: By Attribution 4.0 License
+ * http://creativecommons.org/licenses/by/4.0/
+ *
+ * Battle Theme 2
+ * "Shiny Tech II" Kevin MacLeod (incompetech.com)
+ * Licensed under Creative Commons: By Attribution 4.0 License
+ * http://creativecommons.org/licenses/by/4.0/
+ *
+ * Boss Theme 1
+ * "Harmful or Fatal" Kevin MacLeod (incompetech.com)
+ * Licensed under Creative Commons: By Attribution 4.0 License
+ * http://creativecommons.org/licenses/by/4.0/
+ */
+
+// TODO: actually add sound or whatever lol
